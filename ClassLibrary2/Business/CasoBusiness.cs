b43d506fd15aa0e0afa35ebd47c9ec9cf7202626
Ricardo Acosta <Ricardo Acosta>
2017-05-08 14:08:07 -0600

    Mi Business

---
+++ ClassLibrary2/Business/CasoBusiness.cs
@@ -0,0 +1,81 @@
+﻿using ClassLibrary2.Data;
+using ClassLibrary2.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2.Business
+{
+    public class CasoBusiness
+    {
+        CasoData casoData;
+
+        public CasoBusiness(string conexion)
+        {
+            this.casoData = new CasoData(conexion);
+        }
+
+        public void InsertarCaso(Caso caso)
+        {
+            try
+            {
+                this.casoData.InsertarCaso(caso);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ActualizarCaso(Caso caso)
+        {
+            try
+            {
+                this.casoData.ActualizarCaso(caso);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void EliminarCaso(string numeroExpediente)
+        {
+            try
+            {
+                this.casoData.EliminarCaso(numeroExpediente);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public LinkedList<Caso> ObtenerCasosPorNino(int cedulaNino)
+        {
+            try
+            {
+                return this.casoData.ObtenerCasosPorNino(cedulaNino);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Caso ObtenerCasoPorExpediente(string numeroExpediente)
+        {
+            try
+            {
+                return this.casoData.ObtenerCasoPorExpediente(numeroExpediente);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ ClassLibrary2/Business/NinoBusiness.cs
@@ -0,0 +1,69 @@
+﻿using ClassLibrary2.Data;
+using ClassLibrary2.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2.Business
+{
+    public class NinoBusiness
+    {
+        private NinoData ninoData;
+
+        public NinoBusiness(string conexion)
+        {
+            this.ninoData = new NinoData(conexion);
+        }
+
+        public void InsertarNino(Nino nino)
+        {
+            try
+            {
+                this.ninoData.InsertarNino(nino);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ActualizarNino(Nino nino)
+        {
+            try
+            {
+                this.ActualizarNino(nino);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void EliminarNino(string numeroCedula)
+        {
+            try
+            {
+                this.ninoData.EliminarNino(numeroCedula);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Nino ObtenerNinoPorCedula(int cedulaNino)
+        {
+            try
+            {
+                return this.ninoData.ObtenerNinoPorCedula(cedulaNino);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
